5c418cc3f2311d7e4628d3864fb558b02c5a6f40
first-wraith <s_a_elizarov@mail.ru>
2021-12-04 08:54:14 +0300

    bug fix creating a README file

---
+++ Readme.md
@@ -0,0 +1,37 @@
+# Парсер новостного сайта
+
+------
+Парсер позволяет извлекать главные новости сайта с последующим сохранением в JSON файла.
+
+Для использования парсера необходимо установить необходимые библиотеки:
+
+    beautifulsoup4==4.10.0
+    requests==2.26.0
+
+для этого, скопируйте команду:
+
+```
+python install -r requirements.txt
+```
+
+в terminal/powershell
+
+------
+
+# News site parser
+
+------
+The parser allows you to extract the main news of the site and then save it to a JSON file.
+
+To use the parser, you need to install the necessary libraries:
+
+    beautifulsoup4 == 4.10.0
+    requests == 2.26.0
+
+to do this, copy the command:
+
+```
+python install -r requirements.txt
+```
+
+in terminal / powershell
--- parser.py
+++ parser.py
@@ -3,31 +3,20 @@ import requests
 from bs4 import BeautifulSoup
 import json
 
-file_name = 'www_securitylab_ru'
-
-'''
-URL = "https://www.securitylab.ru/news/"
-
 HEADERS = {
     "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
     "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/15.1 Safari/605.1.15"
 }
+response = requests.get('https://www.securitylab.ru/news/')
+soup = BeautifulSoup(response.text, 'lxml')
+find_index_lastpage = soup.find(class_="page-picker").text.strip().split()
+last_page = int(find_index_lastpage[1])
 
-response = requests.get(url=URL, headers=HEADERS)
-response = response.text
-
-with open("index.html", 'w', encoding='utf-8') as file:
-    file.write(response)
-'''
-
-with open('index.html') as file:
-    file = file.read()
-
-soup = BeautifulSoup(file, 'lxml')
 parser = []
+# for page in range(1,last_page-1):
 for page in range(1, 4):
     link = f'https://www.securitylab.ru/news/page1_{page}.php'
-    response = requests.get(link)
+    response = requests.get(url=link, headers=HEADERS)
     soup = BeautifulSoup(response.text, 'lxml')
     all_url_news = soup.find_all("a", class_="article-card inline-card")
     all_url_list = ('https://www.securitylab.ru' + url.get('href') for url in all_url_news)
@@ -55,6 +44,6 @@ for page in range(1, 4):
             "description": description
         })
 
-with open(f"{file_name}.json", "a", encoding="utf-8") as file:
+with open('www_securitylab_ru.json', "a", encoding="utf-8") as file:
     json.dump(parser, file, indent=4, ensure_ascii=False)
     print('Data collection completed')
